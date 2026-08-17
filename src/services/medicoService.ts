6137bf3714982de79c2d71adcece7586d10eb7e2
Lucas Grecco <lucas.rgrecco@gmail.com>
2026-08-17 09:43:31 -0300

    Feat: adicionando métodos nos services

--- src/services/medicoService.ts
+++ src/services/medicoService.ts
@@ -10,3 +10,32 @@ export async function buscarMedicoPorId(id: number): Promise<Medico> {
  const response = await api.get<Medico>(`/medicos/by-id/${id}`);
  return response.data;
 }
+
+export async function buscarMedicoPorCrm(crm: string): Promise<Medico> {
+    const response = await api.get<Medico>(`/medicos/by-crm/${crm}`);
+    return response.data;
+}
+
+export async function listarMedicosPorEspecialidade(
+    especialidadeId: number
+): Promise<Medico[]> {
+    const response = await api.get<Medico[]>(
+        `/medicos/by-especialidade/${especialidadeId}`
+    );
+    return response.data;
+}
+
+export async function atualizarMedico(
+    id: number,
+    dados: Medico
+): Promise<Medico> {
+    const response = await api.put<Medico>(`update-by-id/${id}`, dados);
+    return response.data;
+}
+
+export async function cadastrarMedico(
+    dados: Omit<Medico, "id">
+): Promise<Medico> {
+    const response = await api.post<Medico>("/medicos", dados);
+    return response.data;
+}
